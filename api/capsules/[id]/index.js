6ef876440e7be2f052425a706546d880b57e18ab
Samarth Asati 19.7 <asatisam053@gmail.com>
2026-09-13 14:33:33 +0530

    Add recipientPhone to PUT request handling

--- api/capsules/[id]/index.js
+++ api/capsules/[id]/index.js
@@ -13,7 +13,7 @@ module.exports = async function handler(req, res) {
   }
 
   if (req.method === 'PUT') {
-    const { recipientName, recipientEmail, deliveryDate, occasion, message, senderName } = req.body || {};
+    const { recipientName, recipientEmail, recipientPhone, deliveryDate, occasion, message, senderName } = req.body || {};
 
     if (!recipientEmail || !deliveryDate || !message) {
       return res.status(400).json({ error: 'recipientEmail, deliveryDate and message are required.' });
@@ -39,6 +39,7 @@ module.exports = async function handler(req, res) {
       ...capsules[index],
       recipientName: recipientName || '',
       recipientEmail,
+      recipientPhone: recipientPhone || '',
       senderName: senderName || '',
       deliveryDate: deliveryTimestamp.toISOString(),
       occasion: occasion || '',
