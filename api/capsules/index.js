65d8fe88f6a5db162937baf4b78fff3a966e4c3a
Samarth Asati 19.7 <asatisam053@gmail.com>
2026-09-14 15:58:44 +0530

    Update index.js

--- api/capsules/index.js
+++ api/capsules/index.js
@@ -1,4 +1,4 @@
-\const { getCapsules, saveCapsules } = require('../../lib/db');
+const { getCapsules, saveCapsules } = require('../../lib/db');
 
 module.exports = async function handler(req, res) {
   if (req.method === 'GET') {
